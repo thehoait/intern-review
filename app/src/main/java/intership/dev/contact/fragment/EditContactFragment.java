51f591129682592e7d5e9d0f331aa126fb68f192
thehoait <thehoait91@gmail.com>
2015-07-23 15:06:10 +0700

    clear code in EditContactFragment

--- app/src/main/java/intership/dev/contact/fragment/EditContactFragment.java
+++ app/src/main/java/intership/dev/contact/fragment/EditContactFragment.java
@@ -1,6 +1,7 @@
 package intership.dev.contact.fragment;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -20,48 +21,61 @@ import intership.dev.contact.widget.CircleImageView;
 /**
  * Created by hoa on 7/22/15.
  */
-public class EditContactFragment extends Fragment {
+public class EditContactFragment extends Fragment implements View.OnClickListener {
+    Intent intent;
+    Contact contact;
+    int position;
+    CircleImageView imgAvatar;
+    TextView tvName;
+    EditText edtName,edtDesc;
+    Button btnSave,btnCancel;
+    ImageView imgBack;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.edit_contact_fragment,container,false);
-        final Intent intent=getActivity().getIntent();
-        final Contact contact=(Contact) intent.getSerializableExtra("contact");
-        final int position=intent.getIntExtra("position",-1);
-        CircleImageView imgAvatar=(CircleImageView) view.findViewById(R.id.imgAvatar);
-        TextView tvName=(TextView) view.findViewById(R.id.tvName);
-        final EditText edtName=(EditText) view.findViewById(R.id.edtName);
-        final EditText edtDesc=(EditText) view.findViewById(R.id.edtDesc);
-        Button btnSave=(Button) view.findViewById(R.id.btnSave);
-        Button btnCancel=(Button) view.findViewById(R.id.btnCancel);
-        ImageView imgBack=(ImageView) view.findViewById(R.id.imgBack);
+        init(view);
+        intent=getActivity().getIntent();
+        contact=(Contact) intent.getSerializableExtra("contact");
+        position=intent.getIntExtra("position",-1);
+
         imgAvatar.setImageResource(contact.getAvatar());
         tvName.setText(contact.getName());
         edtName.setText(contact.getName());
         edtDesc.setText(contact.getDescription());
-        btnCancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getActivity().finish();
-            }
-        });
-        btnSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                contact.setName(edtName.getText().toString());
-                contact.setDescription(edtDesc.getText().toString());
-                intent.putExtra("position",position);
-                intent.putExtra("contact", contact);
-                getActivity().setResult(Activity.RESULT_OK,intent);
-                getActivity().finish();
-            }
-        });
-        imgBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getActivity().onBackPressed();
-            }
-        });
+
+        btnCancel.setOnClickListener(this);
+        btnSave.setOnClickListener(this);
+        imgBack.setOnClickListener(this);
         return view;
+    }
+
+    void init(View view) {
+        imgAvatar=(CircleImageView) view.findViewById(R.id.imgAvatar);
+        tvName=(TextView) view.findViewById(R.id.tvName);
+        edtName=(EditText) view.findViewById(R.id.edtName);
+        edtDesc=(EditText) view.findViewById(R.id.edtDesc);
+        btnSave=(Button) view.findViewById(R.id.btnSave);
+        btnCancel=(Button) view.findViewById(R.id.btnCancel);
+        imgBack=(ImageView) view.findViewById(R.id.imgBack);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id=view.getId();
+        if(id==btnCancel.getId()){
+            getActivity().finish();
+        }
+        if(id==btnSave.getId()){
+            contact.setName(edtName.getText().toString());
+            contact.setDescription(edtDesc.getText().toString());
+            intent.putExtra("position",position);
+            intent.putExtra("contact", contact);
+            getActivity().setResult(Activity.RESULT_OK,intent);
+            getActivity().finish();
+        }
+        if(id==imgBack.getId()){
+            getActivity().finish();
+        }
     }
 }
